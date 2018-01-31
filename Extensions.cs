911b7ec9a9842f1a79e07a9506c51e9eb1e865b3
spersson88 <sebastian.persson@gmail.com>
2018-01-31 11:39:49 +0100

    Create Extensions.cs

---
+++ Extensions.cs
@@ -0,0 +1,132 @@
+static class Extensions
+{
+        /// <summary>
+        /// Replace multiple characters with same value in string.
+        /// </summary>
+        /// <param name="str">Source string</param>
+        /// <param name="newVal">Replace value</param>
+        /// <param name="remove">Characters to replace</param>
+        /// <returns>String with characters replaced by new value.</returns>
+        public static string Replace(this string str, string newVal, params char[] remove)
+        {
+            var temp = str.Split(remove, StringSplitOptions.RemoveEmptyEntries);
+            return string.Join(newVal, temp);
+        }
+
+        /// <summary>
+        /// Check if list contains source object.
+        /// </summary>
+        /// <param name="source">The object we are looking for.</param>
+        /// <param name="list">A list of objects.</param>
+        /// <returns>True if list contains source, otherwise false.</returns>
+        public static bool IsIn<T>(this T source, params T[] list)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            return list.Contains(source);
+        }
+
+        /// <summary>
+        /// Remove last character in string.
+        /// </summary>
+        /// <returns>New string without last character.</returns>
+        public static string RemoveLastCharacter(this string str)
+        {
+            return str.Substring(0, str.Length - 1);
+        }
+
+        /// <summary>
+        /// Remove last X characters in string.
+        /// </summary>
+        /// <param name="length">Number of characters to remove.</param>
+        /// <returns>New string without last X characters.</returns>
+        public static string RemoveLast(this string str, int length)
+        {
+            return str.Substring(0, str.Length - length);
+        }
+
+        /// <summary>
+        /// Remove first character in string.
+        /// </summary>
+        /// <returns>New string without first character.</returns>
+        public static string RemoveFirstCharacter(this string str)
+        {
+            return str.Substring(1);
+        }
+
+        /// <summary>
+        /// Remove first X characters in string.
+        /// </summary>
+        /// <param name="length">Number of characters to remove.</param>
+        /// <returns>New string without first X characters.</returns>
+        public static string RemoveFirst(this string str, int length)
+        {
+            return str.Substring(length);
+        }
+
+        /// <summary>
+        /// Check if date is between start and end date.
+        /// </summary>
+        /// <param name="dt">Date to check.</param>
+        /// <param name="start">Start date</param>
+        /// <param name="end">End date</param>
+        /// <returns>True if date is between start and end dates.</returns>
+        public static bool Between(this DateTime dt, DateTime start, DateTime end)
+        {
+            return dt >= start && dt <= end;
+        }
+
+        /// <summary>
+        /// Check if value is between start and end, exclusive.
+        /// </summary>
+        /// <param name="val">Actual value</param>
+        /// <param name="start">Start value</param>
+        /// <param name="end">End value</param>
+        /// <returns>True if val is larger than start and smaller than end. Otherwise false.</returns>
+        public static bool IsBetween<T>(this T val, T start, T end) where T : IComparable<T>
+        {
+            return val.CompareTo(start) > 0 && val.CompareTo(end) < 0;
+        }
+
+        /// <summary>
+        /// Check if value is between start and end, inclusive.
+        /// </summary>
+        /// <param name="val">Actual value</param>
+        /// <param name="start">Start value</param>
+        /// <param name="end">End value</param>
+        /// <returns>True if val is larger than or equal to start and smaller than or equal to end. Otherwise false.</returns>
+        public static bool IsWithin<T>(this T val, T start, T end) where T : IComparable<T>
+        {
+            return val.CompareTo(start) >= 0 && val.CompareTo(end) <= 0;
+        }
+
+        /// <summary>
+        /// Reverse an array
+        /// </summary>
+        /// <typeparam name="T">Type of array</typeparam>
+        /// <param name="a">Array to reverse</param>
+        public static void Reverse<T>(this T[] a)
+        {
+            for (int i = 0; i < a.Length / 2; i++)
+            {
+                var temp = a[i];
+                a[i] = a[a.Length - 1 - i];
+                a[a.Length - 1 - i] = temp;
+            }
+        }
+        /// <summary>
+        /// Swap two places in array.
+        /// </summary>
+        /// <typeparam name="T">Type of array</typeparam>
+        /// <param name="a">Array to perform swap on.</param>
+        /// <param name="firstIndex">First index to swap.</param>
+        /// <param name="secondIndex">Second index to swap.</param>
+        public static void Swap<T>(this T[] a, int firstIndex, int secondIndex)
+        {
+            var temp = a[firstIndex];
+            a[firstIndex] = a[secondIndex];
+            a[secondIndex] = temp;
+        }
+}
